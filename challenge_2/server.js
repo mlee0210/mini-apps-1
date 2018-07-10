b988e0e23ee4b5dd3a5a812b56384a08c9d8ed23
Michael Lee <lee.cs.michael@gmail.com>
2018-07-10 15:09:54 -0700

    CHALLENGE2: create server and basic html form

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
 
+//Import express
+var express = require('express');
 
+//create app level object of Express JS
+var app = express();
+
+app.post('/', (req, res) => {
+  res.send('POST REQUEST HANDLED');
+});
+
+
+//listen to the desired port 
+app.listen(3000, () => {console.log('Server is running on 3000')});
